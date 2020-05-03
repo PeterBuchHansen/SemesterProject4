44bdbe3176106f6b490252e9eb01b58fdbcdcf40
PeterBuchHansen <60644097+PeterBuchHansen@users.noreply.github.com>
2020-05-03 18:02:51 +0200

    Made robot connection

--- emasg18_kamik18_phans18_simulering3/PLCSolution1/PLCSolution2/Temp/Objects/Config1/X20CP1586/Libraries.h
+++ emasg18_kamik18_phans18_simulering3/PLCSolution1/PLCSolution2/Temp/Objects/Config1/X20CP1586/Libraries.h
@@ -1,8 +1,8 @@
 /* Automation Studio generated header file */
 /* Do not edit ! */
 
-#ifndef _LIBRARIES_20200503125552_
-#define _LIBRARIES_20200503125552_
+#ifndef _LIBRARIES_20200503132916_
+#define _LIBRARIES_20200503132916_
 
 __asm__(".section \".plc\"");
 
@@ -490,5 +490,5 @@ __asm__(".ascii \"iecfile \\\"Logical/Libraries/standard/standard.var\\\" scope 
 __asm__(".previous");
 
 
-#endif /* _LIBRARIES_20200503125552_ */
+#endif /* _LIBRARIES_20200503132916_ */
 
